7479a10e33e09d9fe98634c1a104c5f69f3f58d5
Md Rohit <108532993+mdrohit22@users.noreply.github.com>
2022-07-23 20:04:51 +0530

    Add files via upload

---
+++ question4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+     int sum = 0, i = 1 , n;
+    printf("Enter the value of n\n");
+    scanf("%d", &n);
+
+    while(i<=n)
+    {
+        sum  += i*i;
+        i++;
+    }
+    printf("sum of square of  first N natural is %d\n", sum);
+    
+    return 0;
+}
